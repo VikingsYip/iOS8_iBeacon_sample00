bb441552d87bef2905aef679f9ecf6bc3c59a4dd
tikomo <tikomo@Komorita-no-MacBook-Air.local>
2014-12-11 11:45:22 +0900

    1st commit

--- iBeaconTest00/ViewController.m
+++ iBeaconTest00/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
+
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -17,11 +20,73 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _locationManager = [[CLLocationManager alloc]init];
+    _locationManager.delegate = self;
+    
+    [_locationManager requestAlwaysAuthorization]; // iOS8
+    
+    [self chkBeaconStart];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//
+// ビーコン監視を開始
+//
+-(void) chkBeaconStart{
+    
+    NSString *uuid = @"C768FFC6-B7C4-4079-8EC7-9094AAA19782";
+    CLBeaconRegion *region = [[CLBeaconRegion alloc]initWithProximityUUID:[[NSUUID alloc]initWithUUIDString:uuid] identifier:@"MyBeacon"];
+    [_locationManager startRangingBeaconsInRegion:region];
+    
+}
+
+//
+// ビーコンが検出された場合
+//
+-(void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region{
+    
+    if( beacons.count > 0) {
+        
+        NSLog(@"count = %d", beacons.count);
+        
+        //CLBeacon *beacon = beacons[0];
+        CLBeacon *beacon = beacons.firstObject;
+        
+        
+        
+        NSLog(@"rssi = %d", beacon.rssi);
+        NSLog(@"major = %@", beacon.major);
+        NSLog(@"minor = %@", beacon.minor);
+        NSLog(@"accuracy = %f", beacon.accuracy);
+        
+        if (beacon.proximity == CLProximityUnknown) {
+            NSLog(@"CLProximityUnknown");
+        }else if (beacon.proximity == CLProximityFar) {
+            NSLog(@"CLProximityFar");
+        }else if (beacon.proximity == CLProximityNear) {
+            NSLog(@"CLProximityNear");
+        }else if (beacon.proximity == CLProximityImmediate) {
+            NSLog(@"CLProximityImmediate");
+        }
+        
+        
+        
+    }
+    
+}
+
+//
+// ビーコンが検出できなかった場合
+// 位置情報、BluetoothがOFFのケースでも呼ばれることがある
+//
+-(void)locationManager:(CLLocationManager *)manager rangingBeaconsDidFailForRegion:(CLBeaconRegion *)region withError:(NSError *)error{
+    
 }
 
 @end
